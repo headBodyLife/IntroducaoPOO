47bdca564412041681ade21a7bb51e3864b389dd
headBodyLife <allan.bt167@gmail.com>
2022-01-20 15:44:26 -0300

    nossa primeira função

---
+++ _04_ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(45);
+            Console.WriteLine(resultadoSaque);
+            Console.WriteLine(contaDoBruno.saldo);
+        }
+    }
+}
