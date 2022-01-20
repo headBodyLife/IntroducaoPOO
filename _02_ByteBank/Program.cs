222dddb8ddd385ff9093eed939c55b65eab511c6
headBodyLife <allan.bt167@gmail.com>
2022-01-20 14:30:30 -0300

    valores padroes

---
+++ _02_ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = "Gabriela";
+            conta.saldo = 200;
+            
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.saldo);
+            Console.WriteLine(conta.agencia);
+            Console.WriteLine(conta.numero);
+        }
+    }
+}
