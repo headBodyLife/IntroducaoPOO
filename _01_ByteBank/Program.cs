0ef6b6d5a1f5b113ac6981ef8d360e9e0f41dc26
headBodyLife <allan.bt167@gmail.com>
2022-01-20 14:06:41 -0300

    usando nossa classe

--- _01_ByteBank/Program.cs
+++ _01_ByteBank/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace _01_ByteBank
+﻿using System;
+
+namespace _01_ByteBank
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            new ContaCorrente();
-            
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 856146;
+            contaDaGabriela.saldo = 100;
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: "+contaDaGabriela.agencia);
+            Console.WriteLine("Número :"+contaDaGabriela.numero);
+            Console.WriteLine("Saldo :"+contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo :"+contaDaGabriela.saldo);
+
         }
     }
 }
